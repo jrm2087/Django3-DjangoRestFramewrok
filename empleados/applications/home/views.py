cf5a8bfb15eab7d2329866fd7dc1efecdb738886
José <63377749+jrm2087@users.noreply.github.com>
2021-08-02 15:42:23 -0500

    vistas genericas y modelos

--- empleados/applications/home/views.py
+++ empleados/applications/home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
-from django.views.generic import TemplateView, ListView
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    CreateView)
+
+from .models import Prueba
 
 
 class PruebaView(TemplateView):
@@ -11,3 +16,15 @@ class PruebaListView(ListView):
     template_name = 'home/lista.html'
     context_object_name = 'listaNumeros'
     queryset = ['0', '10', '20', '30']
+
+
+class ListarPrueba(ListView):
+    template_name = 'home/lista_prueba.html'
+    model = Prueba
+    context_object_name = 'lista'
+
+
+class PruebaCreateView(CreateView):
+    template_name = 'home/add.html'
+    model = Prueba
+    fields = ['titulo', 'subtitulo', 'cantidad']
