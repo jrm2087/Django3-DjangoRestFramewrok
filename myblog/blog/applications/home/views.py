4ac92e5a58b7ef7a2c3fa1d9d312ba2d0f7fb1c9
José <63377749+jrm2087@users.noreply.github.com>
2021-10-13 09:51:02 -0500

    pantalla home

--- myblog/blog/applications/home/views.py
+++ myblog/blog/applications/home/views.py
@@ -5,10 +5,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy, reverse
 
 from django.views.generic import (
-    TemplateView
+    TemplateView,
+    CreateView
 )
 
 from applications.entrada.models import Entry
+
+from .models import Home
+from .forms import SuscribersForm
 
 
 class HomePageView(TemplateView):
@@ -16,8 +20,17 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
-
+        # cargamos el home
+        context['home'] = Home.objects.latest('created')
         context['portada'] = Entry.objects.entrada_en_portada()
         context['entradas_home'] = Entry.objects.entradas_en_home()
         context['entradas_recientes'] = Entry.objects.entradas_recientes()
+        # enviar formulario de suscripciones
+        context['form'] = SuscribersForm
+
         return context
+
+
+class SuscriberCreateView(CreateView):
+    form_class = SuscribersForm
+    success_url = '.'
